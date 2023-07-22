e02b5934b6609b733ba1527ae2813ead1cb37c29
KangOxford <kang@robots.ox.ac.uk>
2023-07-22 23:25:02 +0800

    update

--- src/07_2_universal_day_compare=>test.py
+++ src/07_2_universal_day_compare=>test.py
@@ -328,40 +328,40 @@ mean = b.mean()
 
 
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import timedelta, datetime
+
+# Font size variable
+font = 20
+
 # Plotting
-# plt.figure(figsize=(6, 4))
-# plt.figure(figsize=(8, 6))
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(16, 12))
+# plt.figure(figsize=(12, 8))
 
 dates = m.index
 x_axis = pd.to_datetime(dates, format='%Y%m%d')
-# plt.plot(x_axis, m.values.astype(np.float64))
-import matplotlib.dates as mdates
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y')) # Set the x-axis format to display dates
-# plt.gcf().autofmt_xdate() # Rotate the x-axis tick labels to avoid overlap
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
 
 # plot first group with shadow
 plt.plot(x_axis, b, label='Mean of R Squared', color='blue')
 plt.fill_between(x_axis, a, c, color='blue', alpha=0.1)
-# plt.fill_between(range(len(a)), a, c, color='blue', alpha=0.1)
 plt.axhline(mean, color='red', linestyle='-', label='Mean across all dates')
-from datetime import timedelta
-plt.text(x_axis[-1]+timedelta(days=5), mean + 0.01, f"{mean:.2f}", verticalalignment='bottom', horizontalalignment='right', color='red')
-# plt.text(x_axis[-1]-timedelta(days=7), mean - 0.17, f"Mean: {mean:.2f}", verticalalignment='bottom', horizontalalignment='right', color='red')
-# plt.text(x_axis, mean, f"Mean: {mean:.2f}", verticalalignment='bottom', horizontalalignment='right', color='red')
-# plt.text(len(b)+4.5, mean, f"Mean: {mean:.2f}", verticalalignment='bottom', horizontalalignment='right', color='red')
-plt.xlabel("Date")
-plt.ylabel("Out of sample R squared")
-# plt.title("Temporal Analysis from Universal Model: Mean Out-of-Sample R-Squared by Stock")
-plt.legend()
-plt.grid(True)
-from datetime import datetime
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")# Get current timestamp and format it
-filename = f"plot_{timestamp}.png"
-# plt.savefig(path00+filename, bbox_inches='tight')
-plt.savefig(path00+filename, dpi=1200, bbox_inches='tight')
-plt.show()
+plt.text(x_axis[-1]+timedelta(days=5), mean + 0.01, f"{mean:.2f}", verticalalignment='bottom', horizontalalignment='right', color='red', fontsize=font*1.2)
 
+# Adjusting font sizes with the font variable
+plt.xlabel("Date", fontsize=font*1.2)
+plt.ylabel("Out of sample R squared", fontsize=font*1.2)
+plt.xticks(fontsize=font*1.2)
+plt.yticks(fontsize=font*1.2)
+plt.legend(fontsize=font*1.2)
+
+plt.grid(True)
+
+# Save the figure with the generated filename
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+filename = f"plot_{timestamp}.png"
+plt.savefig(path00+filename, dpi=1200, bbox_inches='tight', format='eps')
+plt.show()
 
 
 
