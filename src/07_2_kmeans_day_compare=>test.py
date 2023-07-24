8877e719b06bcdea1149c4a65a6e68d8322b010e
KangOxford <kang@robots.ox.ac.uk>
2023-07-24 21:48:10 +0800

    update

--- src/07_2_kmeans_day_compare=>test.py
+++ src/07_2_kmeans_day_compare=>test.py
@@ -22,13 +22,13 @@ path01Files, path01_1Files, path02Files, path04Files, path05Files, path06Files =
 
 # n_clusters = 2
 # n_clusters = 5
-n_clusters = 10
-# n_clusters = 20
+# n_clusters = 10
+n_clusters = 20
 # n_clusters = 50
 
 
-# ratio_cumsum = 0.80
-ratio_cumsum = 0.99
+ratio_cumsum = 0.80
+# ratio_cumsum = 0.99
 # ratio_cumsum = 0.9999
 # ratio_cumsum = 1.00
 
@@ -130,7 +130,8 @@ def get_features_features(new_dflst_lst):
     return nfeatures
 
 
-features = get_features(new_dflst_lst,type="features")
+features = get_features(new_dflst_lst,type="volume")
+# features = get_features(new_dflst_lst,type="features")
 features.shape
 
 
