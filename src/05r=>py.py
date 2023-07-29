b0576d6a76b609b659865dbd371cf24e302bac14
KangOxford <kang@robots.ox.ac.uk>
2023-07-30 07:43:36 +0800

    update

--- src/05r=>py.py
+++ src/05r=>py.py
@@ -17,35 +17,60 @@ import pandas as pd
 readFromPath = lambda data_path: sorted([f for f in listdir(data_path) if isfile(join(data_path, f)) and f != '.DS_Store'])
 path0400_1Files, path0500Files = map(readFromPath, [path0400_1, path0500])
 
+
+# r2 check
+def r2_check_0400_1():
+    lst = []
+    for i in tqdm(range(len(path0400_1Files))):
+        item = pd.read_csv(path0400_1 + path0400_1Files[i], index_col=0)
+        item['log_qty_shift1']=item.log_qty.shift(-1)
+        import numpy as np
+        item['date'] = np.array([[i]*26 for i in range(item.shape[0]//26)]).reshape(-1)
+        itm = item[:-26]
+        g = itm.groupby("date")
+        for index, it in g:
+            pass
+            true = it.log_qty_shift1.apply(np.exp)
+            pred = it.log_x.apply(np.exp)
+            from sklearn.metrics import r2_score
+            r2 = r2_score(true, pred)
+            lst.append([index, path0400_1Files[i][:-4],r2])
+
+    df = pd.DataFrame(lst,columns=['date','stock','r2'])
+    dff=df.pivot(index = 'date',columns='stock')
+    r2 = dff.mean(axis=1).mean()
+    assert r2 >= 0.45, f"the kf-cmem should have a oos r2:{r2} over 0.45"
+r2_check_0400_1()
+
 from tqdm import tqdm
 for i in tqdm(range(len(path0400_1Files))):
     item = pd.read_csv(path0400_1+path0400_1Files[i],index_col=0)
     item.to_pickle(path0500 + path0400_1Files[i][:-3] +"pkl")
     assert item.shape[0] == 110*26
 
-# r2 check
-lst = []
-for i in tqdm(range(len(path0500Files))):
-    item = pd.read_pickle(path0500 + path0400_1Files[i][:-3] +"pkl")
-    item['log_qty_shift1']=item.log_qty.shift(-1)
-    import numpy as np
-    item['date'] = np.array([[i]*26 for i in range(item.shape[0]//26)]).reshape(-1)
-    itm = item[:-26]
-    g = itm.groupby("date")
-    for index, it in g:
-        pass
-        true = it.log_qty_shift1.apply(np.exp)
-        pred = it.log_x.apply(np.exp)
-        from sklearn.metrics import r2_score
-        r2 = r2_score(true, pred)
-        lst.append([index, path0400_1Files[i][:-4],r2])
+def r2_check_(path,pathFiles):
+    lst = []
+    for i in tqdm(range(len(path0500Files))):
+        item = pd.read_pickle(path0500 + path0400_1Files[i][:-3] +"pkl")
+        item['log_qty_shift1']=item.log_qty.shift(-1)
+        import numpy as np
+        item['date'] = np.array([[i]*26 for i in range(item.shape[0]//26)]).reshape(-1)
+        itm = item[:-26]
+        g = itm.groupby("date")
+        for index, it in g:
+            pass
+            true = it.log_qty_shift1.apply(np.exp)
+            pred = it.log_x.apply(np.exp)
+            from sklearn.metrics import r2_score
+            r2 = r2_score(true, pred)
+            lst.append([index, path0400_1Files[i][:-4],r2])
 
-df = pd.DataFrame(lst,columns=['date','stock','r2'])
-dff=df.pivot(index = 'date',columns='stock')
-r2 = dff.mean(axis=1).mean()
-assert r2 >= 0.45, f"the kf-cmem should have a oos r2:{r2} over 0.45"
+    df = pd.DataFrame(lst,columns=['date','stock','r2'])
+    dff=df.pivot(index = 'date',columns='stock')
+    r2 = dff.mean(axis=1).mean()
+    assert r2 >= 0.45, f"the kf-cmem should have a oos r2:{r2} over 0.45"
 
-
+r2_check_(path0500,path0500Files)
 
 
 
