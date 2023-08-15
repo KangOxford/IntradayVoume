2eb072575262956c01b0bc1836b062c14626b735
KangOxford <kang@robots.ox.ac.uk>
2023-08-15 09:35:10 +0800

    nn numStock

--- codes/nn.py
+++ codes/nn.py
@@ -7,10 +7,10 @@ import torch.nn.functional as F
 numStock = 492
 
 # Sample data
-X_train = torch.randn(100, 260*numStock, 40)
-y_train = torch.randn(100, 26*numStock, 1)
-X_test = torch.randn(100, 260*numStock, 40)  # Test data
-y_test = torch.randn(100, 26*numStock, 1)
+X_train = torch.randn(260*numStock, 40)
+y_train = torch.randn(26*numStock, 1)
+X_test = torch.randn(260*numStock, 40)  # Test data
+y_test = torch.randn(26*numStock, 1)
 
 
 class CNN_LSTM_Model(nn.Module):
@@ -24,33 +24,28 @@ class CNN_LSTM_Model(nn.Module):
 
         self.lstm1 = nn.LSTM(input_size=64, hidden_size=50, num_layers=1, batch_first=True)
 
-        # Adding a max pooling layer to reduce the sequence length
-        self.pool = nn.MaxPool1d(10)  # Pooling with a factor of 10
+        # Adding a max pooling layer to reduce the sequence length by a factor of 10
+        self.pool = nn.MaxPool1d(10)  # Pooling with a kernel size of 10
 
         self.fc = nn.Linear(50, 1)
 
     def forward(self, x):
-        x = x.permute(0, 2, 1)  # Permute to match Conv1d input shape
+        x = x.view(260 * numStock, 40).unsqueeze(0)  # Reshape to (batch_size, sequence_length, features)
+        x = x.permute(0, 2, 1)
         x = self.cnn(x)
-        x = x.permute(0, 2, 1)  # Permute back to match LSTM input shape
+        x = x.permute(0, 2, 1)
         x, _ = self.lstm1(x)
-        x = x.permute(0, 2, 1)  # Permute for pooling
+        x = x.permute(0, 2, 1)
         x = self.pool(x)
-        x = x.permute(0, 2, 1)  # Permute back for fully connected layer
+        x = x.permute(0, 2, 1)
         x = self.fc(x)
-        return x
+        return x.squeeze(0)
 
-
-def test(model, X_test, y_test):
-    with torch.no_grad():
-        outputs = model(X_test)
-        loss = F.mse_loss(outputs, y_test)
-        return loss.item()
-
+# ...
 
 def train():
     # Model, loss function, and optimizer
-    model = CNN_LSTM_Model()
+    model = CNN_LSTM_Model()  # No need to pass numStock
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
@@ -64,8 +59,13 @@ def train():
         optimizer.step()
         print(f'Epoch {epoch + 1}/{epochs}, Loss: {loss.item()}')
     return model
+def test(model, X_test, y_test):
+    with torch.no_grad():
+        outputs = model(X_test)
+        loss = F.mse_loss(outputs, y_test)
+        return loss.item()
 
-    # You can now use the trained model for prediction
+
 if __name__=="__main__":
     trained_model = train()
     test_loss = test(trained_model, X_test, y_test)
