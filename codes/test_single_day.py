e20afa812e0f78c80604627f287c23a0bc634e4b
kang <kang@robots.ox.ac.uk>
2023-08-20 02:47:38 +0000

    update

--- codes/test_single_day.py
+++ codes/test_single_day.py
@@ -15,50 +15,13 @@ import os;os.sys.path.append("/homes/80/kang/cmem/")
 import os;os.sys.path.append("/homes/80/kang/cmem/codes/")
 from utils import *
 from model import *
+from params import *
 import multiprocessing
 import time
 
 path0600_1Files = readFromPath(path0600_1)
 print(len(path0600_1Files))
 
-def param_define():
-    bin_size = 26
-    train_size = 10 * 26
-    test_size = 1 * 26
-    # index_max  = int((df.shape[0] -(train_size + test_size))/bin_size)
-    # index = 0 for index in range(0, index_max+1)
-    # index = 0 for index in range(0, index_max+0) # not sure
-
-    # x_list = ['x', 'eta*seas', 'eta', 'seas', 'mu']
-    # y_list = ['turnover']
-
-    # x_list = ['eta','seas','mu']
-    # y_list = ['turnover']
-
-    our_log_features = ['log_ntn', 'log_volBuyNotional', 'log_volSellNotional', 'log_nrTrades', 'log_ntr',
-                        'log_volBuyNrTrades_lit', 'log_volSellNrTrades_lit', 'log_volBuyQty', 'log_volSellQty',
-                        'log_daily_ntn', 'log_daily_volBuyNotional', 'log_daily_volSellNotional', 'log_daily_nrTrades',
-                        'log_daily_ntr', 'log_daily_volBuyNrTrades_lit', 'log_daily_volSellNrTrades_lit',
-                        'log_daily_volBuyQty', 'log_daily_volSellQty', 'log_daily_qty', 'log_intraday_ntn',
-                        'log_intraday_volBuyNotional', 'log_intraday_volSellNotional', 'log_intraday_nrTrades',
-                        'log_intraday_ntr', 'log_intraday_volBuyNrTrades_lit', 'log_intraday_volSellNrTrades_lit',
-                        'log_intraday_volBuyQty', 'log_intraday_volSellQty', 'log_intraday_qty', 'log_ntn_2',
-                        'log_volBuyNotional_2', 'log_volSellNotional_2', 'log_nrTrades_2', 'log_ntr_2',
-                        'log_volBuyNrTrades_lit_2', 'log_volSellNrTrades_lit_2', 'log_volBuyQty_2', 'log_volSellQty_2',
-                        'log_ntn_8', 'log_volBuyNotional_8', 'log_volSellNotional_8', 'log_nrTrades_8', 'log_ntr_8',
-                        'log_volBuyNrTrades_lit_8', 'log_volSellNrTrades_lit_8', 'log_volBuyQty_8', 'log_volSellQty_8']
-    x_list = ['log_x', 'log_eta*seas', 'log_eta', 'log_seas', 'log_mu']
-    x_list = x_list + our_log_features
-    y_list = ['log_turnover']
-    # x_list = ['log_eta', 'log_seas', 'log_mu']
-    # y_list = ['log_turnover']
-    # x_list = ['x']
-    # y_list = ['turnover']
-
-    # x_list = ['log_x']
-    # y_list = ['log_turnover']
-    original_space = ['turnover']
-    return bin_size, train_size, test_size, x_list, y_list, original_space
 
 
 def process_df(index,regulator):
@@ -126,7 +89,7 @@ def getUniversalDf():
 def getClusterDf():
     return 0
 
-def get_universal(num_of_stocks,regulator):
+def get_r2df(num_of_stocks,regulator):
     global num, df, bin_size, train_size, test_size, x_list, y_list, original_space, total_test_days, num_processes
     num = num_of_stocks
     df = getUniversalDf()
@@ -185,7 +148,7 @@ if __name__=="__main__":
     # regulator = "Ridge"
     # regulator = "None"
     
-    df3 = get_universal(num_of_stocks=len(path0600_1Files),regulator=regulator)
+    df3 = get_r2df(num_of_stocks=len(path0600_1Files),regulator=regulator)
 
 
 
