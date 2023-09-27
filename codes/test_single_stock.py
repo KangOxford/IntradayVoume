13384370952c9068ad8e38d2732edcf430feaff7
kang <kang@robots.ox.ac.uk>
2023-09-27 08:21:49 +0000

    u

--- codes/test_single_stock.py
+++ codes/test_single_stock.py
@@ -15,7 +15,6 @@ import os;os.sys.path.append("/homes/80/kang/cmem/")
 import os;os.sys.path.append("/homes/80/kang/cmem/codes/")
 from utils import *
 from model import *
-from params import *
 from trainPred import *
 from get_results import get_r2df
 import multiprocessing
