4b323123e86a18478c2e72bd0cf58af1120ba3d5
kang <kang@robots.ox.ac.uk>
2024-05-16 11:29:15 +0100

    update universal training

--- codes/get_results.py
+++ codes/get_results.py
@@ -6,7 +6,9 @@ import os;os.sys.path.append(os.path.expanduser('~') + "/cmem/")
 import os;os.sys.path.append("/homes/80/kang/cmem/")
 import os;os.sys.path.append("/homes/80/kang/cmem/codes/")
 from trainPred import *
+from utils import get_git_hash
 from utils import check_GPU_memory
+from tqdm import tqdm 
 
 
 import ray
@@ -33,20 +35,38 @@ def get_r2df(num,regulator,df):
         "short_hash":get_git_hash()
     }
     
+    # # suquentially
+    # start = time.time()
+    # # with multiprocessing.Pool(processes=num_processes) as pool:
+    # r2results = [];oneday_dfs=[]
+    # print("total_test_days",total_test_days)
+    # # index=0
+    # for index in range(total_test_days):     
+    #     try:
+    #         r2result,oneday_df = train_and_pred(index,df,config)     
+    #         r2results.append(r2result)
+    #         oneday_dfs.append(oneday_df)
+    #     except:pass
+    #     # print(r2results)
+    #     # print(oneday_dfs)
+    # end = time.time()
+    
+    # in parallel
     start = time.time()
-    # with multiprocessing.Pool(processes=num_processes) as pool:
-    r2results = [];oneday_dfs=[]
-    print("total_test_days",total_test_days)
-    # index=0
-    for index in range(total_test_days):     
-        try:
-            r2result,oneday_df = train_and_pred(index,df,config)     
-            r2results.append(r2result)
-            oneday_dfs.append(oneday_df)
-        except:pass
-        # print(r2results)
-        # print(oneday_dfs)
+    ray.shutdown()
+    ray.init(num_cpus=64,object_store_memory=40*1e9)
+    @ray.remote
+    def train_and_pred_ray(index,df,config):
+        print(f"+++ date index: {index}")
+        return train_and_pred(index,df,config)
+    ids=[train_and_pred_ray.remote(index,df,config) for index in tqdm(3)]
+    # ids=[train_and_pred_ray.remote(index,df,config) for index in tqdm(range(total_test_days))]
+    results = [ray.get(id_) for id_ in tqdm(ids)]
+    r2results,oneday_dfs=zip(*results)
     end = time.time()
+    print(f"get r2results,oneday_dfs time taken {end-start}")
+    breakpoint()
+    
     
     def get_r2df_from_results(r2results):
         r2arr = np.array(r2results).reshape(-1, 3)
@@ -67,8 +87,8 @@ def get_r2df(num,regulator,df):
 
 
 
-import subprocess
 def get_git_hash():
+    import subprocess
     try:
         # Execute the command to get the latest commit hash
         git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('utf-8')
